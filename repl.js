f4d4047c1111824c7a529c93aabb0fc0f8fb69e7
Bradley Holt <bradley.holt@us.ibm.com>
2017-09-11 13:34:28 -0400

    Provide an app-specific REPL

---
+++ repl.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const repl = require("repl");
+const PouchDB = require("pouchdb-memory");
+const { ShoppingListFactory } = require("./src/ShoppingListFactory");
+const { ShoppingListRepositoryPouchDB } = require("./src/ShoppingListRepositoryPouchDB");
+
+const replServer = repl.start({
+  prompt: "shopping-list > ",
+});
+
+replServer.context.PouchDB = PouchDB;
+replServer.context.ShoppingListFactory = ShoppingListFactory;
+replServer.context.ShoppingListRepositoryPouchDB = ShoppingListRepositoryPouchDB;
