572a7ff8a8e5576f789edd302d1db52dd754bf55
Bradley Holt <bradley.holt@us.ibm.com>
2017-09-12 12:29:34 -0400

    Wire up pouchdb-find plugin in REPL

--- repl.js
+++ repl.js
@@ -2,6 +2,7 @@
 
 const repl = require("repl");
 const PouchDB = require("pouchdb-memory");
+PouchDB.plugin(require("pouchdb-find"));
 const { ShoppingListFactory } = require("./src/ShoppingListFactory");
 const { ShoppingListRepositoryPouchDB } = require("./src/ShoppingListRepositoryPouchDB");
 
