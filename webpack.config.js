6122a40ab30bcfa01bfd273c177638eee4870d4e
Bradley Holt <bradley.holt@us.ibm.com>
2017-09-20 12:25:37 -0400

    Build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "ShoppingListModel"
+  }
+};
